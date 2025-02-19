2ae980af263308ba433f50f9ffca4870909b3fe0
asude-demirci <asudemrci@gmail.com>
2025-02-20 01:07:20 +0300

    Project Done

---
+++ src/store/reducers/favoritesReducer.js
@@ -0,0 +1,31 @@
+import { TOGGLE_FAVORITES, ADD_FAVORITE, REMOVE_FAVORITE } from "../actions/favoritesActions";
+import { DELETE_MOVIE } from "../actions/movieActions"; 
+
+const initialState = {
+    favorites: [], 
+    displayFavorites: false  
+  };
+  const favoritesReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case TOGGLE_FAVORITES:
+        return { ...state, displayFavorites: !state.displayFavorites }; 
+  
+      case ADD_FAVORITE:
+        return { ...state, favorites: [...state.favorites, action.payload] };
+  
+      case REMOVE_FAVORITE:
+        return {
+          ...state,
+          favorites: state.favorites.filter(movie => movie.id !== action.payload)
+        };
+      case DELETE_MOVIE:
+        return {
+         ...state,
+         favorites: state.favorites.filter((movie) => movie.id !== action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default favoritesReducer;
